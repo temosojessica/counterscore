ff8be8d814884922c62daef54b3d515a00376eea
temosojessica <temosojessica@gmail.com>
2018-06-27 11:47:53 -0700

    first commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -7,8 +7,40 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+scoreTeamA = 0;
+  scoreTeamB = 0;
   constructor(public navCtrl: NavController) {
 
   }
+  onReset(){
+    this.scoreTeamA = 0;
+    this.scoreTeamB = 0;
+}
+onClickA(){
+  this.scoreTeamA += 3;
+  console.log(this.scoreTeamA);
 
+}
+
+onTwoA(){
+  this.scoreTeamA += 2;
+  console.log(this.scoreTeamA);
+}
+
+onThrowA(){
+  this.scoreTeamA += 1;
+  console.log(this.scoreTeamA);
+}
+onClickB(){
+  this.scoreTeamB += 3;
+  console.log(this.scoreTeamB);
+}
+onTwoB(){
+  this.scoreTeamB += 3;
+  console.log(this.scoreTeamB);
+}
+onThrowB(){
+  this.scoreTeamB += 3;
+  console.log(this.scoreTeamB);
+}
 }
